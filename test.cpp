e7810368a4cbbbcdc1926d4a6a4e6a7dbf6c2413
MehtabSyed <nadeem.sayeed@gmail.com>
2021-06-06 20:22:30 +0530

    new commit

--- test.cpp
+++ test.cpp
@@ -9,6 +9,10 @@ int main()
 {
     cout << "This is a test:" << endl;
 
+    cout << "This is 2nd commit " << endl;
+
+    cout << "This is testing after git add command" << endl;
+
     return 0;
     
 }
